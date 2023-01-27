1e7c7823542dc55155c62c381ce3213442273977
Jaewon <brand0610@naver.com>
2023-01-27 16:55:44 +0900

    수정작업

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import store from "./reducers/store";
 
 import "./index.css";
 
 import App from "./App";
-
-import store from "./reducer/store";
-
-import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -21,4 +17,3 @@ root.render(
     <App />
   </Provider>
 );
-
--- src/reducer/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-export default configureStore({
-  reducer: {},
-});
-let players = createSlice({
-  name: "players",
-  initialState: [],
-});
