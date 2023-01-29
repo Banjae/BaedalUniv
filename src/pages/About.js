db5509088ffda921ea209b2f63673cf536c5b866
Banjae <brand0610@naver.com>
2023-01-29 19:39:50 +0900

    about 페이지 작업중

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 //  About members page
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // FontAwesome Icon 적용
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
@@ -11,31 +11,6 @@ import tw from "tailwind-styled-components";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const ProfileUl = tw.ul`
-  flex
-  flex-col
-  justify-between
-  items-center
-  `;
-
-const ProfileLi = tw.li`
-  flex
-`;
-
-const Profile = tw.div`
-
-  w-32
-  h-64
-  bg-slate-300
-  shadow-lg
-  rounded-lg
-  m-2
-`;
-
-const Name = tw.div`
-  text-2xl
-`;
-
 const About = () => {
   useEffect(() => {
     AOS.init();
@@ -44,35 +19,42 @@ const About = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div>멤버소개 페이지입니다.</div>
       <button onClick={() => navigate(-1)}>
         <FontAwesomeIcon icon={faChevronLeft} />
       </button>
       <div>
-        <div>FE Members</div>
+        <div className="text-center text-2xl">FE Members</div>
         <ProfileUl>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>반XX</Name>
-              <a href="https://github.com/Banjae" target="blank">
+              <Name>반재원</Name>
+              <PorfileCenter>
+                <ProfilePic></ProfilePic>
+                <ProfileP>
+                  담당파트
+                  <br />
+                  홈 / 상점 
+                </ProfileP>
+              </PorfileCenter>
+              <Link to="https://github.com/Banjae" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>옥XX</Name>
-              <a href="https://github.com/dev-jiji" target="blank">
+              <Name>옥지은</Name>
+              <Link to="https://github.com/dev-jiji" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>조XX</Name>
-              <a href="https://github.com/choj96" target="blank">
+              <Name>조준영</Name>
+              <Link to="https://github.com/choj96" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
         </ProfileUl>
@@ -80,42 +62,42 @@ const About = () => {
         <ProfileUl>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>이XX</Name>
-              <a href="https://github.com/" target="blank">
+              <Name>이영은</Name>
+              <Link to="https://github.com/" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>문XX</Name>
-              <a href="https://github.com/" target="blank">
+              <Name>문주영</Name>
+              <Link to="https://github.com/" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>이XX</Name>
-              <a href="https://github.com/" target="blank">
+              <Name>우민경</Name>
+              <Link to="https://github.com/" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>이XX</Name>
-              <a href="https://github.com/" target="blank">
+              <Name>이민석</Name>
+              <Link to="https://github.com/" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
           <ProfileLi data-aos="zoom-in" data-aos-duration="3000">
             <Profile>
-              <Name>이XX</Name>
-              <a href="https://github.com/" target="blank">
+              <Name>이호진</Name>
+              <Link to="https://github.com/" target="blank">
                 <FontAwesomeIcon icon={faGithub} />
-              </a>
+              </Link>
             </Profile>
           </ProfileLi>
         </ProfileUl>
@@ -123,5 +105,60 @@ const About = () => {
     </>
   );
 };
+
+const ProfileUl = tw.ul`
+  flex
+  flex-col
+  h-auto
+  w-full
+  `;
+
+const ProfileLi = tw.li`
+  flex
+  justify-center
+`;
+
+const Profile = tw.div`
+  flex
+  flex-col
+  items-center
+  justify-center
+  bg-slate-300
+  shadow-lg
+  rounded-lg
+  p-4
+  m-4
+  w-[70%]
+  md:w-[40%]
+  text-[50px]
+  `;
+
+const Name = tw.div`
+  text-3xl
+  mb-5
+`;
+
+const PorfileCenter = tw.div`
+  flex
+  justify-around
+  w-[80%]
+  h-[80%]
+  mb-5
+`;
+
+const ProfileP = tw.p`
+  flex
+  items-center
+  justify-start
+  text-xl
+`;
+
+const ProfilePic = tw.div`
+  w-[200px]
+  h-[200px]
+  rounded-lg
+  bg-main
+  m-1
+`;
 
 export default About;
